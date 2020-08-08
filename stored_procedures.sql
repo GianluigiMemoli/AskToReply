adc7785dd20fe8992f9b321dd08047f48aa20fc7
unknown <carmine.dibenedetto98@gmail.com>
2020-08-08 20:31:56 +0200

    Elenco stored procedure.

---
+++ stored_procedures.sql
@@ -0,0 +1,78 @@
+/*
+    ... = parametri omessi per rapidità
+*/
+
+/*
+    CreazioneUtente(...)
+
+    Per la registrazione di un utente normale e la creazione di un moderatore o master moderatore.
+
+*/
+
+/*
+    GetUtenteByEmail(email)
+    
+    Per l'accesso.
+*/
+
+/*
+    SetNuovaPassword(nuovaPassword)
+
+    Per il recupero password.
+*/
+
+/*
+    GetTendenze()
+
+    Per le tendenze.
+*/
+
+/*
+    UpdateUtente(...)
+
+    Per la modifica del profilo.
+*/
+
+/*
+    GetDomande(idUtente)
+    GetRiposte(idUtente)
+
+    Per lo storico domande/risposte.
+*/
+
+/*
+    PubblicazioneDomanda(...)
+    PubblicazioneRisposta(...)
+
+    Per la pubblicazione di una domanda/risposta.
+*/
+
+/*
+    SegnalazioneDomanda(idDomanda, idMotivazione, commento)
+    SegnalazioneRisposta(idRisposta, idMotivazione, commmento)
+
+    Per la segnalazione di una domanda/risposta.
+*/
+
+/*
+    RicercaDomande(...)
+
+    Per la ricerca con e/o senza filtri.
+*/
+
+/*
+    RimozioneDomanda(idDomanda)
+    RimozioneRisposta(idRisposta)
+*/
+
+/*
+    VotazioneRisposta(idRisposta, valore)
+    RimozioneVotazioneRisposta(idRisposta)
+*/
+
+/*
+    RisolviSegnalazioneDomanda(idSegnalazione, stato)
+    RisolviSegnalazioneRisposta(idSegnalazione, stato)
+
+    stato = risolta, declinata, in attesa.
+*/
