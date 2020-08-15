c0adebd095cc6f29d0490384405793f8e1d0c7c6
bellogradoV <v.bellogrado@studenti.unisa.it>
2020-08-15 18:12:38 +0200

    Caricamento procedura

---
+++ RisolviSegnalazione.sql
@@ -0,0 +1,7 @@
+DELIMITER $$
+CREATE PROCEDURE RisolviSegnalazione(_idSegnalazione VARCHAR(256), _stato INTEGER)
+BEGIN
+	UPDATE Segnalazioni SET stato=_stato
+    WHERE idSegnalazione=_idSegnalazione;
+END;
+DELIMITER;
