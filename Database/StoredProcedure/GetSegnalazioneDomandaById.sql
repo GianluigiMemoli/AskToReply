647016bd1ade0a79c124fa6e200e05b2bfb4d036
bellogradoV <v.bellogrado@studenti.unisa.it>
2020-09-02 16:03:36 +0200

    add SP

---
+++ Database/StoredProcedure/GetSegnalazioneDomandaById.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+CREATE PROCEDURE GetSegnalazioneDomandaById(_idSegnalazione VARCHAR(256))
+BEGIN
+	SELECT Segn.*, SegnDom.idDomanda
+    FROM Segnalazioni AS Segn
+    INNER JOIN segnalazioniDomanda AS SegnDom
+		ON (Segn.id = SegnDom.idSegnalazione)
+    WHERE Segn.stato=0 AND Segn.id=_idSegnalazione
+	ORDER BY Segn.dataSegnalazione ASC;
+END;
+DELIMITER;
