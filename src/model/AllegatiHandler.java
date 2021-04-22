2c062107f59d0b8bb34ae12993c77e70c3d2edfc
Carmine DB <carmine.dibenedetto98@gmail.com>
2021-04-22 13:07:29 +0200

    Classe AllegatiHandler aggiornata. Presenti ora controlli sul numero, sul tipo e sulla dimensione degli allegati.

--- src/model/AllegatiHandler.java
+++ src/model/AllegatiHandler.java
@@ -16,41 +16,80 @@ import java.util.logging.Logger;
 import javax.servlet.http.Part;
 
 public class AllegatiHandler {
-
+	
+	//
+	
+	public final static int NUMERO_MAX_ALLEGATI = 5;
+	
+	public final static long DIMENSIONE_MAX_ALLEGATO = 4 * 1024 * 1024; // 4 MB
+	
+	public final static String[] TIPI_FILE_CONSENTITI = {
+			"image/gif",
+			"image/jpeg",
+			"image/png",
+			"image/bmp",
+			"image/tiff"
+	};
+	
+	//
+	
 	private static final Logger logger = Logger.getLogger(AllegatiHandler.class.getName());
 	
+	//
+	
 	public void caricaAllegati(List<Part> allegati, String destinationFolder) throws Exception {
-		
+			
 		// Ricordare di passare l'id della domanda nella destination folder
 		
-		if(allegati.size() > 0) {
+		// Controllo sul numero di allegati
+		if(allegati.size() > NUMERO_MAX_ALLEGATI)
+			throw new Exception("Numero allegati superiore a " + NUMERO_MAX_ALLEGATI);
+		
+		for (Part filePart : allegati) {
 			
-			File dir = new File(destinationFolder);
+			// Controllo dimensione allegato
+			if(filePart.getSize() >= DIMENSIONE_MAX_ALLEGATO)
+				throw new Exception("Allegato '" + filePart.getSubmittedFileName() + "' ha una dimensione superiore a " + DIMENSIONE_MAX_ALLEGATO + " byte");
+		
+			// Controllo sull'estensione dell'allegato
+			boolean found = false;;
 			
-			if(!dir.exists()) {
-				dir.mkdirs();
+			for(int i = 0; i < TIPI_FILE_CONSENTITI.length; i++) {
+				if(filePart.getContentType().compareTo(TIPI_FILE_CONSENTITI[i]) == 0) {
+					found = true;
+					break;
+				}
 			}
 			
-		    for (Part filePart : allegati) {
-		    	
-		    	// TODO C'è un problema col nome del file: al suo interno è presente anche il suo path assoluto
-		        
-		        String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString();
-		        
-		        InputStream fileContent = filePart.getInputStream();
-		        byte[] data = new byte[fileContent.available()];
-		        fileContent.read(data);
-		        
-		        File file = new File(destinationFolder + File.separator + fileName);
-		        OutputStream out = new FileOutputStream(file);
-		        out.write(data);
-		        out.close();
-		        
-		    }
-		    
-		    
+			if(!found)
+				throw new Exception("Allegato '" + filePart.getSubmittedFileName() + "' ha un estensione non ammessa");
+		
 		}
-	}
+		
+		File dir = new File(destinationFolder);
+		
+		if(!dir.exists()) {
+			dir.mkdirs();
+		}
+		
+	    for (Part filePart : allegati) {
+	    	
+	    	// TODO C'è un problema col nome del file: al suo interno è presente anche il suo path assoluto
+	        
+	        String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString();
+	        
+	        InputStream fileContent = filePart.getInputStream();
+	        byte[] data = new byte[fileContent.available()];
+	        fileContent.read(data);
+	        
+	        File file = new File(destinationFolder + File.separator + fileName);
+	        OutputStream out = new FileOutputStream(file);
+	        out.write(data);
+	        out.close();
+	        
+	    }
+		    
+	}	
 	
 	public File[] getAllegati(String folder) {
 		
@@ -83,7 +122,7 @@ public class AllegatiHandler {
 			Path path = file.toPath();
 			byte[] bytes = Files.readAllBytes(path);
 			String encodedBytes = Base64.getEncoder().encodeToString(bytes);
-			allegatiBase64.add(encodedBytes);	
+			allegatiBase64.add(encodedBytes);
 		}
 		
 		return allegatiBase64;
