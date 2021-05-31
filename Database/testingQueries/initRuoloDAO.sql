7caa6141525c5c23755b218f20d5eb889ac9825b
Carmine DB <carmine.dibenedetto98@gmail.com>
2021-05-31 12:45:25 +0200

    Fix RuoloDAOTest.java + relativi file SQL mancanti

---
+++ Database/testingQueries/initRuoloDAO.sql
@@ -0,0 +1,4 @@
+INSERT INTO ruoli VALUES
+(999997, "Ruolo Testing 1"),
+(999998, "Ruolo Testing 2"),
+(999999, "Ruolo Testing 3");
--- src/testing/RuoloDAOTest.java
+++ src/testing/RuoloDAOTest.java
@@ -2,37 +2,46 @@ package testing;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import model.DBManager;
 import model.RuoloDAO;
 
 class RuoloDAOTest {
 
+	public static final String PATH_INIT_SCRIPT = "Database/testingQueries/initRuoloDAO.sql";
+	public static final String PATH_TEAR_DOWN_SCRIPT = "Database/testingQueries/tearDownRuoloDAO.sql";
+	
 	@BeforeEach
-	public void init() {
-		// TODO
+	public void init() throws IOException, SQLException {
+		DBManager manager = DBManager.getInstance();
+		manager.executeFromScript(PATH_INIT_SCRIPT);
 	}
 	
 	@AfterEach
-	public void clean() {
-		// TODO
+	public void clean() throws IOException, SQLException {
+		DBManager manager = DBManager.getInstance();
+		manager.executeFromScript(PATH_TEAR_DOWN_SCRIPT);
 	}
 	
 	// TODO getRuoloByNome
 	@Test
 	public void getRuoloByNomeTest() {
-		assertNotNull(RuoloDAO.getRuoloByName("Partecipante"));
-		assertNotNull(RuoloDAO.getRuoloByName("Moderatore"));
-		assertNotNull(RuoloDAO.getRuoloByName("MasterModeratore"));
+		assertNotNull(RuoloDAO.getRuoloByName("Ruolo Testing 1"));
+		assertNotNull(RuoloDAO.getRuoloByName("Ruolo Testing 2"));
+		assertNotNull(RuoloDAO.getRuoloByName("Ruolo Testing 3"));
 	}
 	
 	// TODO getRuoloById
 	@Test
 	public void getRuoloByIdTest() {
-		assertNotNull(RuoloDAO.getRuoloById(1));
-		assertNotNull(RuoloDAO.getRuoloById(2));
-		assertNotNull(RuoloDAO.getRuoloById(3));
+		assertNotNull(RuoloDAO.getRuoloById(999997));
+		assertNotNull(RuoloDAO.getRuoloById(999998));
+		assertNotNull(RuoloDAO.getRuoloById(999999));
 	}
 }
