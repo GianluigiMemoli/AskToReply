3b6d451ea13a15a65a575062f80811b6076745e5
Gianluigi Memoli <g.memoli98@gmail.com>
2021-07-05 11:12:30 +0200

    demo.sql

--- Database/demo.sql
+++ Database/demo.sql
@@ -212,3 +212,6 @@ insert into categoriedomande values("DOM1ID","id-categoria-demo-8"),
                                    ("DOM2ID","id-categoria-demo-9"),
 								   ("DOM2ID","id-categoria-demo-1"),
 								   ("DOM3ID","id-categoria-demo-5");
+
+insert into segnalazioni VALUES("id-segnalazione-demo-1", 1, CURDATE(), 1, "Questa domanda è offensiva", "id-utente-demo-3");
+insert into segnalazionidomanda values("id-segnalazione-demo-1", "DOM3ID");
